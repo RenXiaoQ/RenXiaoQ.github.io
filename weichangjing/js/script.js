5b4385de99f6b3119a294a5b538f67a493865f5e
RenXiaoQ <847285679@qq.com>
2017-12-12 17:30:48 +0800

    新增

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -25,7 +25,9 @@ var mp3btn=document.querySelector("#mp3btn");
 mp3btn.addEventListener("click",function(){
     if(mp3.paused){
         mp3.play()
+        mp3btn.style.animationPlayState="running"
     }else {
         mp3.pause()
+        mp3btn.style.animationPlayState="paused"
     }
 })
